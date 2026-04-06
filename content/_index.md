b9327fe6bd3333babce423bea162033518da69cb
Nathan Breneman <nathan@natb.dev>
2026-04-06 12:32:23 -0700

    Recreate repo after build

---
+++ content/_index.md
@@ -0,0 +1,8 @@
++++
+title = "Home"
+template = "index.html"
++++
+
+# Nathan Breneman
+
+Senior Software Engineer
---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Zola static site generator development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs allSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [ zola ];
+          };
+        }
+      );
+    };
+}
